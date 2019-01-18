d1da136b6ba0f769e381e16b5a41c764d4b1b866
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2019-01-18 16:24:23 +0100

    fix: switch to RS256 algorithm

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,5 +1,16 @@
 require('dotenv').config();
+const jwks = require('jwks-rsa');
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 4000,
+  JWT: {
+    secret: jwks.expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: 'https://rtt-extension.eu.auth0.com/.well-known/jwks.json',
+    }),
+    issuer: process.env.AUTH0_ISSUER,
+    algorithms: ['RS256'],
+  },
 };
